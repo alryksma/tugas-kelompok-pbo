d4aae1ba6a860e60d87c1a13ce01b4e4e5782ecc
aliflosthimself <alifarya679@gmail.com>
2023-12-10 01:01:38 +0700

    menambahkan akses modifier

--- index.py
+++ index.py
@@ -2,28 +2,28 @@ class Animal:
     def __init__(self, nama, sifat , ukuran, jumlahKaki):
         self.nama = nama
         self.sifat = sifat
-        self.ukuran = ukuran
-        self.jumlahKaki = jumlahKaki
+        self._ukuran = ukuran
+        self._jumlahKaki = jumlahKaki
 
 class Mamalia(Animal):
     def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaJalan, jenisMamalia):
         super().__init__(nama, sifat, ukuran, jumlahKaki)
         self.bisaJalan = bisaJalan
-        self.jenisMamalia = jenisMamalia
+        self.__jenisMamalia = jenisMamalia
 
 
 class Aves(Animal):
     def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves):
         super().__init__(nama, sifat, ukuran, jumlahKaki)
         self.bisaTerbang = bisaTerbang
-        self.jenisAves = jenisAves
+        self.__jenisAves = jenisAves
 
         
 class Ayam(Aves):
     def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves, jenisAyam, bisaDiadu):
         super().__init__(nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves)
         self.jenisAyam = jenisAyam
-        self.bisaDiadu = bisaDiadu
+        self.__bisaDiadu = bisaDiadu
 
 class Merpati(Aves):
     def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves):
