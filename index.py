15e97bc97420778cb62a47c90f95c64f038f4d1c
aliflosthimself <alifarya679@gmail.com>
2023-12-12 14:04:33 +0700

    mengubah file index menjadi lebih rinci

--- index.py
+++ index.py
@@ -1,31 +1,46 @@
 class Animal:
-    def __init__(self, nama, sifat , ukuran, jumlahKaki):
+    def __init__(self, nama, sifat, ukuran, kaki):
         self.nama = nama
         self.sifat = sifat
-        self._ukuran = ukuran
-        self._jumlahKaki = jumlahKaki
+        self.ukuran = ukuran
+        self.kaki = kaki
+
 
 class Mamalia(Animal):
-    def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaJalan, jenisMamalia):
-        super().__init__(nama, sifat, ukuran, jumlahKaki)
-        self.bisaJalan = bisaJalan
-        self.__jenisMamalia = jenisMamalia
+    def __init__(self, nama, sifat, ukuran, kaki, bisa_jalan, jenis_mamalia):
+        super().__init__(nama, sifat, ukuran, kaki)
+        self.bisa_jalan = bisa_jalan
+        self.jenis_mamalia = jenis_mamalia
 
 
 class Aves(Animal):
-    def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves):
-        super().__init__(nama, sifat, ukuran, jumlahKaki)
-        self.bisaTerbang = bisaTerbang
-        self.__jenisAves = jenisAves
+    def __init__(self, nama, sifat, ukuran, kaki, bisa_terbang, jenis_aves):
+        super().__init__(nama, sifat, ukuran, kaki)
+        self.bisa_terbang = bisa_terbang
+        self.jenis_aves = jenis_aves
 
-        
+
 class Ayam(Aves):
-    def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves, jenisAyam, bisaDiadu):
-        super().__init__(nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves)
-        self.jenisAyam = jenisAyam
-        self.__bisaDiadu = bisaDiadu
+    def __init__(
+        self,
+        nama,
+        sifat,
+        ukuran,
+        kaki,
+        bisa_terbang,
+        jenis_aves,
+        jenis_ayam,
+        bisa_diadu,
+    ):
+        super().__init__(nama, sifat, ukuran, kaki, bisa_terbang, jenis_aves)
+        self.jenis_ayam = jenis_ayam
+        self.bisa_diadu = bisa_diadu
 
-class Merpati(Aves):
-    def __init__(self, nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves):
-        super().__init__(nama, sifat, ukuran, jumlahKaki, bisaTerbang, jenisAves)
 
+# Contoh penggunaan
+ayam_pertama = Ayam(
+    "Ayam Putih", "Ramah", "Sedang", 2, True, "Burung", "Ayam Kampung", True
+)
+print(
+    f"{ayam_pertama.nama} adalah sejenis {ayam_pertama.jenis_ayam} dan {'bisa' if ayam_pertama.bisa_diadu else 'tidak bisa'} diadu."
+)
