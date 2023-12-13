bf4394fd1ae5c265c77836741ce3badf08617114
aliflosthimself <alifarya679@gmail.com>
2023-12-13 15:13:35 +0700

    memperbaiki file python

--- index.py
+++ index.py
@@ -5,42 +5,32 @@ class Animal:
         self.ukuran = ukuran
         self.kaki = kaki
 
-
 class Mamalia(Animal):
     def __init__(self, nama, sifat, ukuran, kaki, bisa_jalan, jenis_mamalia):
         super().__init__(nama, sifat, ukuran, kaki)
         self.bisa_jalan = bisa_jalan
         self.jenis_mamalia = jenis_mamalia
-
-
+    def tampil(self):
+        print(f'Nama hewan: {self.nama}, sifat hewan: {self.sifat}, 
+              ukuran: {self.ukuran}, jumlah kaki: {self.kaki}, 
+              bisa jalan: {self.bisa_jalan}, jenis mamalia: {self.jenis_mamalia}') 
+    
 class Aves(Animal):
     def __init__(self, nama, sifat, ukuran, kaki, bisa_terbang, jenis_aves):
         super().__init__(nama, sifat, ukuran, kaki)
         self.bisa_terbang = bisa_terbang
         self.jenis_aves = jenis_aves
-
-
+        
+    def tampil(self):
+        print(f'Nama hewan: {self.nama}, sifat hewan: {self.sifat}, 
+              ukuran: {self.ukuran}, jumlah kaki: {self.kaki}, 
+              bisa jalan: {self.bisa_terbang}, jenis mamalia: {self.jenis_aves}')
 class Ayam(Aves):
-    def __init__(
-        self,
-        nama,
-        sifat,
-        ukuran,
-        kaki,
-        bisa_terbang,
-        jenis_aves,
-        jenis_ayam,
-        bisa_diadu,
-    ):
+    def __init__(self, nama, sifat, ukuran, kaki, bisa_terbang, jenis_aves, jenis_ayam, bisa_diadu):
         super().__init__(nama, sifat, ukuran, kaki, bisa_terbang, jenis_aves)
         self.jenis_ayam = jenis_ayam
         self.bisa_diadu = bisa_diadu
 
-
 # Contoh penggunaan
-ayam_pertama = Ayam(
-    "Ayam Putih", "Ramah", "Sedang", 2, True, "Burung", "Ayam Kampung", True
-)
-print(
-    f"{ayam_pertama.nama} adalah sejenis {ayam_pertama.jenis_ayam} dan {'bisa' if ayam_pertama.bisa_diadu else 'tidak bisa'} diadu."
-)
+ayam_pertama = Ayam("Ayam Putih", "Ramah", "Sedang", 2, True, "Burung", "Ayam Kampung", True)
+print(f"{ayam_pertama.nama} adalah sejenis {ayam_pertama.jenis_ayam} dan {'bisa' if ayam_pertama.bisa_diadu else 'tidak bisa'} diadu.")
